c11d0b0e259bfd33409dd9ee2aa5c0e256501cad
Radoslav Radoev <rradoev@gmail.com>
2022-08-01 12:22:04 -0700

    number of good pairs

--- Easy/numberOfGoodPairs.js
+++ Easy/numberOfGoodPairs.js
@@ -20,3 +20,24 @@ Example 3:
 Input: nums = [1,2,3]
 Output: 0
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var numIdenticalPairs = function(nums) {
+  let goodPairs = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        goodPairs += 1;
+      }
+    }
+  }
+  return goodPairs;
+};
+
+// const nums = [1,2,3,1,1,3];
+// const nums = [1,1,1,1];
+const nums = [1,2,3];
+console.log(numIdenticalPairs(nums));
